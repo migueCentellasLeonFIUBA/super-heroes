7ffee38a0b671594a06923073633f7f5e9457e73
mcentellas <mcentellas@technisys.com>
2022-12-28 23:31:41 -0300

    Agregado de APiOperation para Swagger y un breve README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mindata : Challenge SuperHeroes
+
+Prueba Tecnica Spring boot
+
+##  Ejecucion de la aplicacion
+
+```bash
+$ java -jar super-heroes-0.0.1-SNAPSHOT.jar
+```
+
+## Documentacion tecnica (Swagger)
+
+endpoint : localhost:8001/swagger-ui.html
+
+## Docker
+
+Puede ser ejecutado via Dockerfile o docker-Compose
--- src/main/java/com/ejercicio/mindata/superheroes/controller/SuperHeroeController.java
+++ src/main/java/com/ejercicio/mindata/superheroes/controller/SuperHeroeController.java
@@ -3,13 +3,20 @@ package com.ejercicio.mindata.superheroes.controller;
 import com.ejercicio.mindata.superheroes.logging.LogExecutionTime;
 import com.ejercicio.mindata.superheroes.model.SuperHeroe;
 import com.ejercicio.mindata.superheroes.service.SuperHeroeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "SuperHeroe Controller", produces = MediaType.APPLICATION_JSON_VALUE, tags = {
+        "SuperHeroe Management" })
 @RestController
 @RequestMapping("/api/superHeroes")
 public class SuperHeroeController {
@@ -20,12 +27,14 @@ public class SuperHeroeController {
         this.superHeroeService = superHeroeService;
     }
 
+
     @GetMapping
     @LogExecutionTime
     public List<SuperHeroe> listarSuperHeroes(){
         return superHeroeService.getAllSuperHeroes();
     }
 
+    @ApiOperation(value = "Obtener SuperHeroes por superHeroeId")
     @GetMapping("/{id}")
     @LogExecutionTime
     public ResponseEntity<?> obtenerSuperHeroePorId(@PathVariable("id") long superHeroeId){
@@ -34,12 +43,14 @@ public class SuperHeroeController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @ApiOperation(value = "Obtener SuperHeroes por palabra",notes = "Ingresar palbra clave a buscar en SuperHeroes")
     @GetMapping("porPalabra/{palabra}")
     @LogExecutionTime
     public List<SuperHeroe> obtenerSuperHeroePorPalabra(@PathVariable("palabra") String palabra){
         return superHeroeService.getSuperHeroePorPalabra(palabra);
     }
 
+    @ApiOperation(value = "Actualizar SupeHeroe", notes = "Actualizar SuperHeroe a traves de otro similar con diferentes atributos")
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     @LogExecutionTime
@@ -49,6 +60,8 @@ public class SuperHeroeController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @ApiOperation(value = "Eliminar SupeHeroe por id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "SuperHeroe eliminado exitosamente") })
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     @LogExecutionTime
